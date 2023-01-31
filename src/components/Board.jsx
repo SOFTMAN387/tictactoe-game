abbaf579bdc77e4775fd79ede7576c7b134fa41f
Manish Kumar Gupta <manishkumarpatna2020@gmail.com>
2023-01-31 13:52:01 +0530

    Added Player's history

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,61 +1,70 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Square from './Square';
 import { calWinner } from './WinnerCal';
+import PlayerHistory from './PlayerHistory';
 import "./style.css";
 const Board = () => {
-  const [board,setBoard]=useState(Array(9).fill(null));
-  const [isXnext,setNext]=useState(false);
-  const winner=calWinner(board);
-  const message=winner?`The Winner is ${winner}`:`Next Player is ${isXnext ?"X":"O"}`
-  //console.log(winner);
-  const handleSquareClick=(position)=>{
-    if(board[position] ||winner){
+  const [history, setHistory] = useState([{ board: Array(9).fill(null), isXnext: true }]);
+  // const [isXnext,setNext]=useState(false);
+  const [currentMove, setCurrentMove] = useState(0);
+  const current = history[currentMove]
+  const winner = calWinner(current.board);
+  const message = winner ? `The Winner is ${winner}` : ( history.length===10?"Opps Match Tie..!":`Next Player is ${current.isXnext ? "X" : "O"}`)
+ // console.log(history);
+  const handleSquareClick = (position) => {
+    if (current.board[position] || winner) {
       return;
     }
-    setBoard(pre=>{
-      return pre.map((square,pos)=>{
-        if(pos===position){
-          return isXnext ?"X":"O";
+    setHistory(pre => {
+      const last = pre[pre.length - 1];
+      const newBoard = last.board.map((square, pos) => {
+        if (pos === position) {
+          return last.isXnext ? "X" : "O";
         }
         return square;
-      })
+      });
+      return pre.concat({ board: newBoard, isXnext: !last.isXnext })
     });
-    setNext(prev=>!prev);
+    setCurrentMove(prev => prev + 1);
   }
 
-  const renderSquare=(position)=>{
-    return <Square value={board[position]}
-    onClick={()=>handleSquareClick(position)} />
+  const renderSquare = (position) => {
+    return <Square value={current.board[position]}
+      onClick={() => handleSquareClick(position)} />
+  }
+
+  const moveTo=(move)=>{
+    setCurrentMove(move);
   }
   //console.log(board);
   return (
-  <>
-    <div className='main-container'>
-    <h3>TIC TAC TOE</h3>
-    <h4>{message}</h4>
+    <>
+      <div className='main-container'>
+        <h3>TIC TAC TOE</h3>
+        <h4><strong><i>{message}</i></strong></h4>
 
-      <div className='middle-container'>
-   
-       <div className='row-container'>
+        <div className='middle-container'>
+
+          <div className='row-container'>
             {renderSquare(0)}
-            {renderSquare(1)} 
+            {renderSquare(1)}
             {renderSquare(2)}
-        </div>
+          </div>
           <div className='row-container'>
             {renderSquare(3)}
-            {renderSquare(4)} 
+            {renderSquare(4)}
             {renderSquare(5)}
-           </div>
-        <div className='row-container'>
+          </div>
+          <div className='row-container'>
             {renderSquare(6)}
-            {renderSquare(7)} 
+            {renderSquare(7)}
             {renderSquare(8)}
 
+          </div>
         </div>
-    </div>
-       
-    </div>
-  </>
+        <PlayerHistory history={history} moveTo={moveTo} currentMove={currentMove}/>
+      </div>
+    </>
   )
 }
 
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./style.css";
 const Square = ({value,onClick}) => {
+
+  console.log(value);
     //console.log(props);
   return (
    <>
-    <button type='button' onClick={onClick} className='row-btn'>{value}</button>
+    <button type='button' style={{color:value==="X"?'blue':'red'}}  onClick={onClick} className='row-btn'>{value}</button>
    </>
   )
 }
